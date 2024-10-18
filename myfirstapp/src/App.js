d2aaf5405d302685860d1c6d7901ac220d6a10ee
Le Nguyen Duy Phuc <157189808+lenguyenduyphuc@users.noreply.github.com>
2024-10-18 18:34:32 -0400

    updated

---
+++ myfirstapp/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App () {
+
+  const currentDate = new Date()
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <h2>The time now is {currentDate.toLocaleTimeString()}.</h2>
+    </div>
+  )
+}
+
+export default App;
